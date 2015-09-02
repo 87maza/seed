112023531b5f8620cd26800255689aa52b44a1f7
87maza <87maza@gmail.com>
2015-09-02 12:51:18 -0700

    added horiz scroller and hover animations

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// window.onscroll = function()
+// {
+//     var nav = document.getElementsByTagName('NAV')
+    
+//     if (nav.scrollTop <=0  || self.pageYOffset <=60) {
+//       nav.style.position = 'fixed';
+//         nav.style.top = '0px';
+//     } else if (nav.scrollTop >= 1 || self.pageYOffset >= 61) {
+//       nav.style.position = 'absolute';
+//     }
+  
+// }
+
+
